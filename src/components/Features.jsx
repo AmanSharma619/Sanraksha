f058ba206eab2049cdaa7ea9721bae5e09986cdb
aman sharma <amansharma.as536@gmail.com>
2025-03-17 00:33:27 +0530

    dashboard layout

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,93 +1,29 @@
-import { Shield, MapPin, AlertCircle, FileText, Bell } from 'lucide-react';
-import { Link } from 'react-router-dom';
-
+import React from 'react'
+import "./Features.css"
+import { useEffect } from 'react'
 const Features = () => {
-  const features = [
-    {
-      path: "heatmaps",
-      icon: <Shield size={32} />,
-      img: "https://i.postimg.cc/5yhd4RD3/heatmaps.png",
-      title: "Safety Ranking",
-      description: "Get detailed safety scores and rankings for different areas in your neighborhood",
-    },
-    {
-      path: "safezones",
-      icon: <MapPin size={32} />,
-      img: "https://i.postimg.cc/HjDGm1kv/Designer-7.jpg",
-      title: "Nearby Safety Zones",
-      description: "Locate and navigate to the nearest safe zones and emergency points",
-    },
-    {
-      path: "recent_crimes",
-      icon: <AlertCircle size={32} />,
-      img: "https://i.postimg.cc/284MWJ6m/preview.webp",
-      title: "Recent Crimes",
-      description: "Stay informed with real-time updates on recent incidents in your area",
-    },
-    {
-      path: "feedback",
-      icon: <FileText size={32} />,
-      img: "https://i.postimg.cc/YqnTfDVd/Screenshot-89.png",
-      title: "Community Feedback",
-      description: "Submit and track safety concerns through our interactive feedback system",
-    },
-    {
-      path: "precautionary_measures",
-      icon: <Bell size={32} />,
-      img: "https://i.postimg.cc/fRwJYGwh/Designer-2.jpg",
-      title: "Precautionary Measures",
-      description: "Access guidelines and tips for maintaining personal and community safety",
-    },
-  ];
-
+  useEffect(()=>{
+   var t=gsap.timeline()
+   t.from("#first",{width:"0px",duration:0.8,opacity:0})
+   t.from("#second",{height:"0px",duration:0.8,opacity:0})
+   t.from("#fifth",{height:"0px",duration:0.8,opacity:0})
+   t.from("#fourth",{ scaleX: 0,duration:0.8,opacity:0,  transformOrigin: "100% 50%"})
+   t.from("#third",{ scaleX: 0,duration:0.8,opacity:0,  transformOrigin: "100% 50%"})
+  },[])
   return (
-    <div className="bg-gradient-to-b from-clr5 via-clr4 to-clr5 h-auto py-12">
-      <div className="px-8">
-        <div className="flex flex-wrap justify-center gap-32">
-          {features.slice(0, 3).map((feature, index) => (
-            <div
-              key={index}
-              className="bg-white/10 border group border-clr4 hover:border-clr2 backdrop-blur-lg rounded-xl p-5 hover:transform hover:scale-105 transition-all duration-300 w-80 h-72 "
-            >
-
-              {/* <div className="flex items-center justify-center w-24 h-10  bg-clr1 text-white mb-4"> */}
-              <img src={feature.img} alt='image' className='object-fill w-full h-36 rounded-xl mb-1 group-hover:blur-md ' />
-              {/* </div> */}
-
-              <button className=' text-md text-clr1 font-bold hover:bg-gradient-to-r from-clr2 to-clr3 hover:text-clr5 rounded-full transition duration-300 ease-in-out   absolute px-4 py-4 left-1/3 top-28 opacity-0 group-hover:opacity-100  '>
-                <Link to={`/${feature.path}`} className="block w-full h-full text-center">
-                  Check Out
-                </Link>
-              </button>
-
-              <h3 className="text-xl font-bold text-clr1 mb-2  group-hover:blur-sm">{feature.title}</h3>
-              <p className="text-clr2  group-hover:blur-sm">{feature.description}</p>
-            </div>
-          ))}
-        </div>
-        <div className="flex justify-center gap-32 mt-16">
-          {features.slice(3).map((feature, index) => (
-            <div
-              key={index}
-              className="bg-white/10 group border border-clr4 bg-contain bg-center hover:border-clr2 backdrop-blur-lg rounded-xl p-6 hover:transform hover:scale-105 transition-all duration-300 w-80 h-72"
-            >
-              {/* <div className="flex items-center justify-center w-12 h-12 rounded-full bg-clr1 text-white mb-4">
-                {feature.icon}
-              </div> */}
-              <img src={feature.img} alt='image' className='object-fill w-full h-36 rounded-xl mb-1 group-hover:blur-md ' />
-              <button className=' text-md text-clr1 font-bold hover:bg-gradient-to-r from-clr2 to-clr3 hover:text-clr5 rounded-full transition duration-300 ease-in-out   absolute px-4 py-4 left-1/3 top-28 opacity-0 group-hover:opacity-100  '>
-                <Link to={`/${feature.path}`} className="block w-full h-full text-center">
-                  Check Out
-                </Link>
-              </button>
-              <h3 className="text-xl font-bold text-clr1 mb-2  group-hover:blur-sm">{feature.title}</h3>
-              <p className="text-clr2  group-hover:blur-sm">{feature.description}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+    <div
+    class="flex h-screen w-full flex-col items-center justify-center rounded-lg p-6"
+  >
+    <h1 className='text-white mb-9'>Safety Dashboard</h1>
+    <div class="grid h-full w-full grid-cols-3 grid-rows-6 gap-4">
+      <div class="col-span-2 row-span-3 rounded-3xl box" id='first'></div>
+      <div class="row-span-4 rounded-3xl box" id='second'></div>
+      <div class="row-span-3 rounded-3xl box" id='third'></div>
+      <div class="row-span-3 rounded-3xl box" id='fourth'></div>
+      <div class="row-span-2 rounded-3xl box" id='fifth'></div>
     </div>
-  );
-};
+  </div>
+  )
+}
 
-export default Features;
+export default Features
